5c1ced500312caef635fb965e2c51fd7c8613826
Edwin Kam <64185146+EdwinKam@users.noreply.github.com>
2024-01-21 11:21:00 -0800

    added create new request page (#1)
    * added create new request page
    
    * fix input box size
    
    ---------
    
    Co-authored-by: ekam <ekam@netflix.com>

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import FormHelperText from "@mui/material/FormHelperText";
+
+interface DropdownProps {
+  value: string;
+  label: string;
+  options: string[];
+  handleChange: (event: SelectChangeEvent) => void;
+  error?: boolean;
+}
+
+export default function Dropdown({
+  value,
+  label,
+  options,
+  handleChange,
+  error,
+}: DropdownProps) {
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth error={error}>
+        <InputLabel id="demo-simple-select-label">{label}</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={value}
+          label={label}
+          onChange={handleChange}
+        >
+          {options.map((o) => (
+            <MenuItem value={o}>{o}</MenuItem>
+          ))}
+        </Select>
+        {error && <FormHelperText>{label} is required</FormHelperText>}
+      </FormControl>
+    </Box>
+  );
+}
