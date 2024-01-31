e08addf5dc3f14ee4cd19bc337931ffec54aacf3
ekam <ekam@netflix.com>
2024-01-30 23:35:48 -0800

    merge conflict

--- src/controllers/BlackjackHttpController.ts
+++ src/controllers/BlackjackHttpController.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { GameResult } from "../models/blackjackTypes";
 
 // const url = 'https://blackjack-service-render.onrender.com/blackjack';
 const url = "http://localhost:8080/blackjack";
@@ -33,73 +32,6 @@ export const simulateRequest = async (
     }
   } catch (error) {
     console.error(`Error in simulateRequest: ${error}`);
-    throw error;
-  }
-};
-
-export const getSimulateResult = async (
-  trackingUuid: string
-): Promise<GameResult> => {
-  try {
-    const response = await axios.post(
-      `${url}/checkResult`,
-      {},
-      {
-        params: {
-          trackingUuid: trackingUuid,
-        },
-      }
-    );
-
-    if (response.status === 200) {
-      return response.data;
-    } else {
-      throw new Error(`Request failed with status code ${response.status}`);
-    }
-  } catch (error) {
-    console.error(`Error in getSimulateResult: ${error}`);
-    throw error;
-  }
-};
-
-export const getBatchSimulateStatus = async (
-  trackingUuids: string[]
-): Promise<Map<string, string>> => {
-  if (trackingUuids === null) {
-    return new Map();
-  }
-  const response = await axios.post(
-    url + "/batchCheckProgress/",
-    trackingUuids.map((uuid) => uuid),
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
-  if (response.status === 200) {
-    return response.data;
-  } else {
-    throw new Error(`Request failed with status code ${response.status}`);
-  }
-};
-
-export const getAllRequests = async (): Promise<Map<string, any>> => {
-  try {
-    const response = await axios.get(`${url}/getAllTrackingUuid`, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.status === 200) {
-      const resultMap = new Map(Object.entries(response.data));
-      return resultMap;
-    } else {
-      throw new Error(`Request failed with status code ${response.status}`);
-    }
-  } catch (error) {
-    console.error(`Error in getAllTrackingUuid: ${error}`);
     throw error;
   }
 };
